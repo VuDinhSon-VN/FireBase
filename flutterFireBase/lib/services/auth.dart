d7a7fc87b2f2e901c8207b0979f8af65e1a0743e
VuDinhSon-VN <sonvudinh96@gmail.com>
2020-07-13 10:06:26 +0700

    git update

---
+++ flutterFireBase/lib/services/auth.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//sign in anon
+Future signInAnon() async
+
+//sign in with email & pass
+
+// register with email & pass
+
+// sign out
+
+}
