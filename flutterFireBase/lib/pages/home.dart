9f32da481881617e6fb60897edae4ab720e7915b
VuDinhSon-VN <sonvudinh96@gmail.com>
2020-07-31 16:47:46 +0700

    Update Home.dart

--- flutterFireBase/lib/pages/home.dart
+++ flutterFireBase/lib/pages/home.dart
@@ -1,26 +1,149 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+import 'activity_feed.dart';
+import 'profile.dart';
+import 'search.dart';
+import 'timeline.dart';
+import 'upload.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-final isAuth = false;
-
-Scaffold buildAuthScreen(){
-
-
-} 
-
-Scaffold buildUnAuthScreen(){
-  
-}
-
 class _HomeState extends State<Home> {
-   @override
+  bool isAuth = false;
+  PageController pageController;
+  int pageIndex = 0;
+  GoogleSignIn googleSignIn = GoogleSignIn();
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+
+    // detects when usser signed in
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account);
+    }, onError: (err) {
+      print('Error signing in: $err');
+    });
+    // reAuthenticate user when app is opened
+    googleSignIn
+        .signInSilently(suppressErrors: false)
+        .then((account) => handleSignIn(account))
+        .catchError(
+          (err) => print('Error signing in: $err'),
+        );
+  }
+
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      print('User signed in!: $account');
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  onPageChanged(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  onTap(int pageIndex) {
+    pageController.animateToPage(pageIndex,
+        duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+
+  Scaffold buildAuthScreen() {
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+          Timeline(),
+          ActivityFeed(),
+          Upload(),
+          Search(),
+          Profile(),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar:
+          CupertinoTabBar(currentIndex: pageIndex, onTap: onTap, items: [
+        BottomNavigationBarItem(icon: Icon(Icons.whatshot)),
+        BottomNavigationBarItem(icon: Icon(Icons.notifications_active)),
+        BottomNavigationBarItem(
+            icon: Icon(
+          Icons.photo_camera,
+          size: 35.0,
+        )),
+        BottomNavigationBarItem(icon: Icon(Icons.search)),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle)),
+      ]),
+    );
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Theme.of(context).accentColor,
+              Theme.of(context).primaryColor,
+            ])),
+        alignment: Alignment.center,
+        child: Column(
+          children: <Widget>[
+            Text(
+              'FlutterFireBase',
+              style: TextStyle(
+                fontFamily: "Signatra",
+                fontSize: 90.0,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: login,
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/google_signin_button.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
